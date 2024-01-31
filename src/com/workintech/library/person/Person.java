f94daa36e9d54b49b982d29224c58249a67d0507
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-02-01 02:44:04 +0300

    Person abstract class is created

---
+++ src/com/workintech/library/person/Person.java
@@ -0,0 +1,44 @@
+package com.workintech.library.person;
+
+import java.util.Objects;
+
+public abstract class Person {
+    private String name;
+    private int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public abstract void whoyouare();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
